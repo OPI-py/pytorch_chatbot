c90962346b55712b2718ab7d5fbd535efddf9eda
OPI-py <oleksandrp09@gmail.com>
2021-03-04 19:17:56 +0200

    Remove excessive code

--- chat_bot.py
+++ chat_bot.py
@@ -51,40 +51,5 @@ def get_response(message):
                 return func.day_today()
             if tag == intent['tag']:
                 return random.choice(intent["responses"])
-                # if intent["responses"][0].startswith('funtion'):
-                    # return func.current_time()
-                # else:
     else:
        return 'I do not undestand, friend:('
-
-# print("I am your friend! Speak with me!")
-
-# while True:
-    # quit_words = ['exit', 'quit', 'leave']
-    # sentence = input("You: ")
-    # if sentence.lower()in quit_words:
-        # print(bot_name + ': Until next time!')
-        # break
-    # sentence = tokenize(sentence)
-    # bow = bag_of_words(sentence, all_words) # create numpy_array
-    # bow = bow.reshape(1, bow.shape[0]) # reshape to 1 row and 0 columns
-    # bow = torch.from_numpy(bow) # creates a Tensor from a numpy.ndarray
-    
-    # output = model(bow) # get predictions
-    # return max prediction value and index of tag
-    # _max_p, predicted = torch.max(output, dim=1)
-    # tag = tags[predicted.item()] # get tag name
-    
-    # get probabilities and probability of predicted item
-    # probs = torch.softmax(output, dim=1)
-    # prob = probs[0][predicted.item()]
-    
-    # if prob.item() > 0.75:
-        # for intent in intents['intents']:
-            # if tag == intent['tag']:
-                # if intent["responses"][0].startswith('print'):
-                    # exec(intent["responses"][0])
-                # else:
-                    # print(f'{bot_name}: {random.choice(intent["responses"])}')
-    # else:
-        # print(f'{bot_name}: I do not undestand, friend:(')
