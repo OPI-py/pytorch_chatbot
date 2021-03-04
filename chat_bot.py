b3d6c1d72c4bae35f8d4720422baf8afc0ff2223
OPI-py <oleksandrp09@gmail.com>
2021-03-04 19:15:32 +0200

    Adjust code for tkinter ui output

--- chat_bot.py
+++ chat_bot.py
@@ -3,7 +3,7 @@ import json
 import torch
 from model import NeuralModel
 from nltk_func import bag_of_words, tokenize
-import datetime
+import func
 
 # check if GPU Compute Unified Device Architecture available
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -26,16 +26,10 @@ model = NeuralModel(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = 'Friend'
-print("I am your friend! Speak with me!")
+bot_name = 'Maartenok'
 
-quit_words = ['exit', 'quit', 'leave']
-while True:
-    sentence = input("You: ")
-    if sentence.lower()in quit_words:
-        print(bot_name + ': Until next time!')
-        break
-    sentence = tokenize(sentence)
+def get_response(message):
+    sentence = tokenize(message)
     bow = bag_of_words(sentence, all_words) # create numpy_array
     bow = bow.reshape(1, bow.shape[0]) # reshape to 1 row and 0 columns
     bow = torch.from_numpy(bow) # creates a Tensor from a numpy.ndarray
@@ -48,13 +42,49 @@ while True:
     # get probabilities and probability of predicted item
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
-    
+        
     if prob.item() > 0.75:
         for intent in intents['intents']:
+            if tag == 'current_time':
+                return func.current_time()
+            elif tag == 'day_today':
+                return func.day_today()
             if tag == intent['tag']:
-                if intent["responses"][0].startswith('print'):
-                    exec(intent["responses"][0])
-                else:
-                    print(f'{bot_name}: {random.choice(intent["responses"])}')
+                return random.choice(intent["responses"])
+                # if intent["responses"][0].startswith('funtion'):
+                    # return func.current_time()
+                # else:
     else:
-        print(f'{bot_name}: I do not undestand, friend:(')
+       return 'I do not undestand, friend:('
+
+# print("I am your friend! Speak with me!")
+
+# while True:
+    # quit_words = ['exit', 'quit', 'leave']
+    # sentence = input("You: ")
+    # if sentence.lower()in quit_words:
+        # print(bot_name + ': Until next time!')
+        # break
+    # sentence = tokenize(sentence)
+    # bow = bag_of_words(sentence, all_words) # create numpy_array
+    # bow = bow.reshape(1, bow.shape[0]) # reshape to 1 row and 0 columns
+    # bow = torch.from_numpy(bow) # creates a Tensor from a numpy.ndarray
+    
+    # output = model(bow) # get predictions
+    # return max prediction value and index of tag
+    # _max_p, predicted = torch.max(output, dim=1)
+    # tag = tags[predicted.item()] # get tag name
+    
+    # get probabilities and probability of predicted item
+    # probs = torch.softmax(output, dim=1)
+    # prob = probs[0][predicted.item()]
+    
+    # if prob.item() > 0.75:
+        # for intent in intents['intents']:
+            # if tag == intent['tag']:
+                # if intent["responses"][0].startswith('print'):
+                    # exec(intent["responses"][0])
+                # else:
+                    # print(f'{bot_name}: {random.choice(intent["responses"])}')
+    # else:
+        # print(f'{bot_name}: I do not undestand, friend:(')
